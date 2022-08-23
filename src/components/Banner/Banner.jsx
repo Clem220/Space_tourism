36418468982b61a98f969861b490139b0f994625
Clément <clement.vermeulen57@gmail.com>
2022-08-23 21:59:26 +0200

    fin de mise en page desktop

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,42 +1,50 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 import Logo from "../../assets/shared/logo.svg";
 import "./Banner.scss";
-const NavContainer = styled.nav`
-  width: 100vw;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-const LinkContainer = styled.div`
-  width: 50vw;
-  display: flex;
-  justify-content: space-around;
-  flex-grow: 0;
-  margin: 0 0 0 507px;
-  padding: 40px;
-  -webkit-backdrop-filter: blur(81.5px);
-  backdrop-filter: blur(81.5px);
-  background-color: rgba(255, 255, 255, 0.04);
-`;
-const NavLink = styled(Link)`
-  color: #fff;
-`;
 
 function Banner() {
   return (
     <header>
-      <NavContainer>
+      <nav className="nav">
         <div>
-          <img src={Logo} alt="logo" />
+          <img className="nav__logo" src={Logo} alt="logo" />
         </div>
-        <LinkContainer>
-          <NavLink to="/">00 Home</NavLink>
-          <NavLink to="/destination">01 Destination</NavLink>
-          <NavLink to="/crew">02 Crew</NavLink>
-          <NavLink to="/technology">03 Technology</NavLink>
-        </LinkContainer>
-      </NavContainer>
+          <div className="nav__border"></div>
+        <div className="nav__linkContent">
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "nav__link-active" : "nav__link"
+            }
+            to="/"
+          >
+            00 HOME
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "nav__link-active" : "nav__link"
+            }
+            to="/destination"
+          >
+            01 DESTINATION
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "nav__link-active" : "nav__link"
+            }
+            to="/crew"
+          >
+            02 CREW
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "nav__link-active" : "nav__link"
+            }
+            to="/technology"
+          >
+            03 TECHNOLOGY
+          </NavLink>
+        </div>
+      </nav>
     </header>
   );
 }
